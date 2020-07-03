fca5c21d7bb04b3eff2d9503243266ef30b65895
Ashish Mishra <asmishra@groupon.com>
2020-07-03 18:57:01 +0530

    inital version of code after initializing git repo

---
+++ src/main/java/com/myretail/MyRetailAPIService.java
@@ -0,0 +1,13 @@
+package com.myretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRetailAPIService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRetailAPIService.class, args);
+	}
+
+}
---
+++ src/test/java/com/myretail/ProductapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
